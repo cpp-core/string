05844b048e002f63ddbcfbf1218f2f5299baf222
Mark Melton <mark.melton@notlem.com>
2019-02-20 20:00:41 -0600

    Added string replace_all.

---
+++ include/core/string/replace.h
@@ -0,0 +1,22 @@
+// Copyright (C) 2019 by Mark Melton
+//
+
+#pragma once
+#include "core/common.h"
+
+namespace core
+{
+
+namespace detail
+{
+string replace_all_base(string_view str, string_view from, string_view to);
+};
+
+template<class T, class U, class... Ts>
+string replace_all(string_view str, T from, U to, Ts&&... rest)
+{
+    if constexpr (sizeof...(Ts) == 0) return detail::replace_all_base(str, from, to);
+    else return replace_all(detail::replace_all_base(str, from, to), std::forward<Ts>(rest)...);
+}
+
+}; // end ns core
