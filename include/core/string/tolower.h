c5ce78d7a851562769327be72c17467a4b251b7c
Mark Melton <mark.melton@notlem.com>
2019-10-16 09:09:21 -0400

    Added string::tolower and associated tests.

---
+++ include/core/string/tolower.h
@@ -0,0 +1,13 @@
+// Copyright (C) 2019 by Mark Melton
+//
+
+#pragma once
+#include "core/common.h"
+
+namespace core
+{
+
+void tolower(string& str);
+string tolower(string_view str);
+
+}; // end ns core
